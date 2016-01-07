e356a464e0d3f33481d6243a52cc09038a28c4f7
Shen Lijia <shenlijia@shendemacbook-pro.local>
2016-01-07 21:47:52 +0800

    Stanford cs 193p 4

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -44,6 +44,26 @@ class CalculatorBrain {
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
     }
     
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList { // guaranteed to be a PropertyList
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let oprand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(oprand))
+                    }
+                }
+            }
+        }
+    }
+    
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         if !ops.isEmpty {
             var remainingOps = ops
